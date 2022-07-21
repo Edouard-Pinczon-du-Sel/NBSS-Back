cd18a19a63403f71c75fb75aeeeb2e6e7b8eb6f9
VirginieBoissiere <boissiere.virginie@gmail.com>
2022-07-21 16:55:34 +0200

    feat navbar: base.html.twig

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,6 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
