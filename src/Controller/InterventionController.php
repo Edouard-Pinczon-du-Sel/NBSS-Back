a1fb40f7892e7dabea5f9bcf7d59b1228e541b36
Gatechien <tuquet.adrien@gmail.com>
2022-07-22 15:34:06 +0200

    test security ok

--- src/Controller/InterventionController.php
+++ src/Controller/InterventionController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/intervention")
+ * @Route("/back/intervention")
  */
 class InterventionController extends AbstractController
 {
--- src/Controller/RecruitmentController.php
+++ src/Controller/RecruitmentController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/recruitment")
+ * @Route("/back/recrutement")
  */
 class RecruitmentController extends AbstractController
 {
