61e11188c3961c334b8317a2d3dec2dbdf7d1885
Gatechien <tuquet.adrien@gmail.com>
2022-07-27 10:21:55 +0200

    ok api

--- src/Controller/Api/PictureController.php
+++ src/Controller/Api/PictureController.php
@@ -4,7 +4,6 @@ namespace App\Controller\Api;
 
 use App\Entity\Picture;
 use App\Repository\PictureRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
